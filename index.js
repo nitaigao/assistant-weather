b4c002f1641e5a855d82ad2481b8352142d6b0fc
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-05-06 17:26:01 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var http        = require('http'),
+    request     = require('request'),
+    querystring = require('querystring');
+
+
+function createServer(port) {
+  http.createServer(function (req, res) {
+    var body = "";
+
+    req.on('data', function (chunk) {
+      body += chunk;
+    })
+
+    req.on('end', function () {
+      res.writeHead(200, {'Access-Control-Allow-Origin': '*'})
+      res.end('OK!');
+
+      var formData = JSON.parse(body);
+      processCommand(formData)
+    });
+
+  }).listen(port)
+}
+
+function start() {
+  createServer(8101)
+}
+
+start()
+
+function processCommand(command) {
+  console.log(command)
+
+  var weather = require('weather');
+  weather({logging: true, appid:'Q2_Ky4zV34FoqoNxkluqSyzvnSWiyyZhc3v5EVRdqXdutumqGZbWdm_qcxFfcNnLmA--', location: 'Esher'}, function(data) {
+    console.log(data);
+
+    request.get("http://localhost:4000?say=" + querystring.escape(data.text))
+  });
+}
