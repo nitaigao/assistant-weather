d4808617f811a1fd404bfe18948b9205c7d929ba
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-07-15 10:18:24 +0100

    using faye

--- index.js
+++ index.js
@@ -1,54 +1,32 @@
-var http        = require('http'),
-    request     = require('request'),
-    querystring = require('querystring'),
-    weather     = require('weather')
-    say         = require('say')
-    ;
-
-function createServer(port) {
-  http.createServer(function (req, res) {
-    var body = "";
-
-    req.on('data', function (chunk) {
-      body += chunk;
-    })
-
-    req.on('end', function () {
-      res.writeHead(200, {'Access-Control-Allow-Origin': '*'})
-      res.end('OK!');
-
-      if (body.length > 0) {
-        var formData = JSON.parse(body);
-        processCommand(formData)
-      }
-    });
-
-  }).listen(port)
-}
+var querystring = require('querystring'),
+    weather     = require('weather'),
+    faye        = require('faye'),
+    settings    = require('env-settings')
+;
 
 function start() {
-  var port = Number(process.env.PORT || 8181);
-  console.log("Started on port " + port)
-  createServer(port)
+  var client = new faye.Client(settings.brain + '/');
+  client.subscribe('/weather', function(message) {
+    var command = JSON.parse(message);
+    processCommand(command);
+  });
 }
 
 start()
 
 function processCommand(command) {
-  console.log(command)
-
   weather({logging: true, appid:'Q2_Ky4zV34FoqoNxkluqSyzvnSWiyyZhc3v5EVRdqXdutumqGZbWdm_qcxFfcNnLmA--', location: 'Esher'}, function(data) {
-    console.log(data);
+    var client = new faye.Client(settings.brain + '/');
 
-      if (command.category == "weather") {
-        var summary = "The weather is currently " + querystring.escape(data.text.toLowerCase())
-        say.message(summary)
-      }
+    if (command.category == "weather") {
+      var summary = "The weather is currently " + data.text
+      client.publish('/responses', summary);
+    }
 
-      if (command.category == "temperature") {
-        var temperature = "The temperature is currently " + querystring.escape(data.temp) + " degrees C"
-        say.message(temperature)
-      }
+    if (command.category == "temperature") {
+      var temperature = "The temperature is currently " + data.temp + " degrees C"
+      client.publish('/responses', temperature);
+    }
 
   });
 }
